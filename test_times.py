923f6fb3aef7fcda574b850eb6dafe6c03af3f80
Hanyu Xu <hanyuxu78@gmail.com>
2023-11-01 15:38:02 +0000

    test code

--- test_times.py
+++ test_times.py
@@ -2,3 +2,9 @@
 
 from times import compute_overlap_time
 
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    result = compute_overlap_time(large,short)
+    expected = "{'start': '2010-01-12 10:30:00', 'end': '2010-01-12 10:45:00'}"
+    assert result == expected
